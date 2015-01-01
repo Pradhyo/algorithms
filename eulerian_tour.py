ec9f4a2adfefc8d19359f894aa243543fe27f566
Pradhyo Bijja <pradhyo@nyu.edu>
2015-01-01 16:14:32 +0530

    Add main function and put all stuff in there

--- eulerian_tour.py
+++ eulerian_tour.py
@@ -55,29 +55,32 @@ def find_eulerian_tour(graph):
     
     return path
 
-graph = [(1, 2), (1, 4), (3, 4), (2, 3), (3, 8), (4, 8), (4, 5), (3,6), (5,6), (7,8), (6,7),(6,8)]
-print "Graph 1 is" 
-print graph
-print "Eulerian tour is" 
-print find_eulerian_tour(graph)
-print
-graph = [(8, 16), (8, 18), (16, 17), (18, 19),
-(3, 17), (13, 17), (5, 13),(3, 4), (0, 18), (3, 14), (11, 14),
-(1, 8), (1, 9), (4, 12), (2, 19),(1, 10), (7, 9), (13, 15),
-(6, 12), (0, 1), (2, 11), (3, 18), (5, 6), (7, 15), (8, 13), (10, 17)]
-print "Graph 2 is" 
-print graph
-print "Eulerian tour is" 
-print find_eulerian_tour(graph)
-print
-graph = [(1, 13), (1, 6), (6, 11), (3, 13),
-(8, 13), (0, 6), (8, 9),(5, 9), (2, 6), (6, 10), (7, 9),
-(1, 12), (4, 12), (5, 14), (0, 1),  (2, 3), (4, 11), (6, 9),
-(7, 14),  (10, 13)]  
-print "Graph 3 is" 
-print graph
-print "Eulerian tour is" 
-print find_eulerian_tour(graph)
 
+def main():
+    print "Examples.."
+    graph = [(1, 2), (1, 4), (3, 4), (2, 3), (3, 8), (4, 8), (4, 5), (3,6), (5,6), (7,8), (6,7),(6,8)]
+    print "Graph 1 is" 
+    print graph
+    print "Eulerian tour is" 
+    print find_eulerian_tour(graph)
+    print
+    graph = [(8, 16), (8, 18), (16, 17), (18, 19),
+    (3, 17), (13, 17), (5, 13),(3, 4), (0, 18), (3, 14), (11, 14),
+    (1, 8), (1, 9), (4, 12), (2, 19),(1, 10), (7, 9), (13, 15),
+    (6, 12), (0, 1), (2, 11), (3, 18), (5, 6), (7, 15), (8, 13), (10, 17)]
+    print "Graph 2 is" 
+    print graph
+    print "Eulerian tour is" 
+    print find_eulerian_tour(graph)
+    print
+    graph = [(1, 13), (1, 6), (6, 11), (3, 13),
+    (8, 13), (0, 6), (8, 9),(5, 9), (2, 6), (6, 10), (7, 9),
+    (1, 12), (4, 12), (5, 14), (0, 1),  (2, 3), (4, 11), (6, 9),
+    (7, 14),  (10, 13)]  
+    print "Graph 3 is" 
+    print graph
+    print "Eulerian tour is" 
+    print find_eulerian_tour(graph)
 
-
+if __name__ == "__main__":
+    main()
