317210a12c4856f40b6cff75e4c3b474e6c50153
Pradhyo Bijja <pradhyo@nyu.edu>
2015-01-01 10:06:02 +0530

    Add code for eulerian tour

--- eulerian_tour.py
+++ eulerian_tour.py
@@ -21,12 +21,39 @@ def find_eulerian_tour(graph):
     nodes = adj.keys()
     print adj
     print nodes
+    total_edges = len(graph)
+    current_edges = 0
     path.append(nodes[0])
-    path.append(adj[nodes[0]][0])
-    print path
+    path_index = 0
+    while (current_edges < total_edges):
+        if current_edges == 0:
+            path.append(adj[path[path_index]][0])
+            path_index += 1
+            current_edges += 1
+            adj[path[path_index-1]].remove(path[path_index])
+            adj[path[path_index]].remove(path[path_index-1])
+
+        if (len(adj[path[path_index]]) == 1):
+            path.append(adj[path[path_index]][0])
+            path_index += 1
+            current_edges += 1
+            adj[path[path_index-1]].remove(path[path_index])
+            adj[path[path_index]].remove(path[path_index-1])
+        
+
+        elif (current_edges != total_edges -2):
+            if (len(adj[adj[path[path_index]][0]]) == 2) and ((adj[adj[path[path_index]][0]][0] == path[0]) or (adj[adj[path[path_index]][0]][1] == path[0])):
+                path.append(adj[path[path_index]][0])
+            else:
+                path.append(adj[path[path_index]][1])
+            path_index += 1
+            current_edges += 1
+            adj[path[path_index-1]].remove(path[path_index])
+            adj[path[path_index]].remove(path[path_index-1])
+        print path
+        
     
-    
-    return []
+    return path
 
 graph = [(1, 2), (1, 4), (3, 4), (2, 3), (3, 8), (4, 8), (4, 5), (3,6), (5,6), (7,8), (6,7),(6,8)]
 find_eulerian_tour(graph)
