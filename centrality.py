8bb738bf89ed0ecb265fa074e8e6fa918a99d3d0
Pradhyo Bijja <pradhyo@nyu.edu>
2015-01-06 13:21:54 +0530

    Add code to find centrality_max

--- centrality.py
+++ centrality.py
@@ -18,7 +18,18 @@ def centrality(G,v):
 
 def centrality_max(G, v):
     # your code here
-    return 0
+    distance_from_start = {}
+    open_list = [v]
+    distance_from_start[v] = 0
+    while  len(open_list) > 0:
+        current = open_list[0]
+        del open_list[0]
+        for neighbor in G[current].keys():
+            if neighbor not in distance_from_start:
+                distance_from_start[neighbor] = distance_from_start[current] + 1
+                open_list.append(neighbor)
+    return max(distance_from_start.values())
+
 
 #################
 # Testing code
@@ -49,3 +60,5 @@ def test():
         make_link(G, n1, n2)
     assert centrality_max(G, 1) == 3
     assert centrality_max(G, 11) == 6
+
+test()
